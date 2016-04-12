9915601d1c3341b48d7e9bbdc8377524785f1f2d
Samuel Ekman <samuel.ekman@hotmail.com>
2016-04-12 10:14:30 +0200

    Created initial model

---
+++ server/booking/model/FishingSafari.java
@@ -0,0 +1,21 @@
+package server.booking.model;
+
+public class FishingSafari{
+    private boolean minParticipantsMet;
+    private boolean maxParticipantsMet;
+    private Date announcementDate;
+    //bookinglist
+    //equipmentlist
+    private int nrParticipants;
+    private Date timeLimit;
+    private boolean assignmentPlanSent;
+
+
+
+
+
+
+
+
+
+}//End of class
---
+++ server/booking/model/SafariDestination.java
@@ -0,0 +1,24 @@
+package server.booking.model;
+
+public class SafariDestination{
+    private String location;
+    private int maxParticipants;
+    private String equipmentReq;
+    private String guide;
+    private boolean active;
+
+
+
+
+
+    // private regChange(){
+    // }
+
+
+
+
+
+
+
+
+}//End of class
