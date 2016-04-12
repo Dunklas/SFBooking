d09123ea37a3b0b86779ab37f54ee93b430ed77b
Dunklas <dunklas@gmail.com>
2016-04-12 13:38:09 +0200

    Added db insert of fishing safari, and a method for it in the model

--- server/booking/model/FishingSafariCatalog.java
+++ server/booking/model/FishingSafariCatalog.java
@@ -1,14 +1,24 @@
 package server.booking.model;
 
 import java.util.*;
+import java.sql.Date;
+import server.utils.Insert;
 
 public class FishingSafariCatalog{
+    
     private List<FishingSafari> safariList = new ArrayList<>();
 
+    public void newFishingSafari(SafariDestination destination, java.sql.Date startDate, java.sql.Date endDate) {
 
+	String safariDestination = destination.getLocation();
 
-
-
+	try {
+	    Insert dbEntry = new Insert();
+	    dbEntry.insertFishingSafari(safariDestination, startDate, endDate);
+	} catch (Exception ex) {
+	    System.out.println("Nu blev det fel");
+	}
+    }
 
     public void cancelFishingSafari(int id, boolean active){
 
--- server/utils/Insert.java
+++ server/utils/Insert.java
@@ -1,11 +1,11 @@
 package server.utils;
 
 import java.sql.*;
+import java.time.*;
 
 public class Insert {
 
     public void insertSafariDestination(String location, String equipmentReq, int maxParticipants, String guide, String terrain) throws SQLException {
-
 	Connection c = new OpenDb().getConnection();
 	if (c != null) {
 	    PreparedStatement stmt = c.prepareStatement("INSERT INTO safaridestination VALUES (?, ?, ?, ?, ?);");
@@ -14,6 +14,20 @@ public class Insert {
 	    stmt.setString(3, equipmentReq);
 	    stmt.setString(4, guide);
 	    stmt.setString(5, location);
+	    stmt.executeUpdate();
+
+	    stmt.close();
+	    c.close();
+	}
+    }
+
+    public void insertFishingSafari(String safariDestination, Date startDate, Date endDate) throws SQLException {
+	Connection c = new OpenDb().getConnection();
+	if (c != null) {
+	    PreparedStatement stmt = c.prepareStatement("INSERT INTO fishingsafari (safaridestination, endDate, startDate) VALUES (?, ?, ?);");
+	    stmt.setString(1, safariDestination);
+	    stmt.setDate(2, startDate);
+	    stmt.setDate(3, endDate);
 	    stmt.executeUpdate();
 
 	    stmt.close();
