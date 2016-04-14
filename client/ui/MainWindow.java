468e7c6eef11e834251a2985d9453d2d75506eb3
christoffermagnusson <christoffer88@live.se>
2016-04-14 09:57:12 +0200

    Updatet testwindow with splitview

--- client/ui/MainWindow.java
+++ client/ui/MainWindow.java
@@ -3,19 +3,29 @@ package client.ui;
 import javax.swing.*;
 import server.booking.control.*;
 import server.booking.model.*;
+import java.sql.*;
+import java.util.*;
 
 
 public class MainWindow {
 
 
     public static void main (String [] args){
-	SafariDestinationCatalog sdc = new SafariDestinationCatalog();
-
-	SafariDestinationView view = new SafariDestinationView();
-	SafariDestinationController sdcontroller = new SafariDestinationController(view, sdc);
+    	SafariDestinationView sdv = new SafariDestinationView();
+    	SafariDestinationCatalog sdc = new SafariDestinationCatalog();
+        ModifySafariDestinationView msdv = new ModifySafariDestinationView();
+        try{
+        msdv.fillList(sdc.selectAllSafariDestination());
+        }
+        catch(SQLException e){
+        	//
+        }
+        MainSplitView split = new MainSplitView(msdv,sdv);
+	
+	SafariDestinationController sdcontroller = new SafariDestinationController(sdv, sdc,msdv);
 
 	JFrame jf = new JFrame();
-	jf.add(view);
+	jf.add(split);
 	jf.setVisible(true);
 	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
