2478ba5fcf14286cbd306843e88c98c7fde9ffff
Dunklas <dunklas@gmail.com>
2016-04-21 09:21:57 +0200

    Further stuff in the domain model of customer/booking

---
+++ server/customer/model/CustomerRegister.java
@@ -0,0 +1,20 @@
+package server.customer.model;
+
+import java.util.ArrayList;
+
+public class CustomerRegister {
+
+    private ArrayList<Customer> customerList;
+
+    public CustomerRegistered() {
+	customerList = new ArrayList<>();
+    }
+
+    public void newCustomer(String firstName, String lastName, String email, String telephone) {
+
+    }
+
+    public boolean checkCustomer(String email) {
+
+    }
+}
