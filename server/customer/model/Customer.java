1c086ce9e2bb9aadaf98a33d10c57980bf282900
Dunklas <dunklas@gmail.com>
2016-04-21 09:10:37 +0200

    Started with domain model for Customer/Booking

---
+++ server/customer/model/Customer.java
@@ -0,0 +1,19 @@
+package server.customer.model;
+
+import java.sql.Date;
+
+public class Customer {
+
+    private int customerID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+
+    private Date registered;
+    private Date unregistered;
+
+    public Customer (int customerID, String firstName, String lastName, String email, String telephone, Date registered) {
+
+    }
+}
