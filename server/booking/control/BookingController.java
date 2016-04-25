225f6c4e19e73dcd9046e9b594b26f4dfe08fe84
FatalPortrait <alexander.moraitis@gmail.com>
2016-04-25 11:58:49 +0200

    lite booking

--- server/booking/control/BookingController.java
+++ server/booking/control/BookingController.java
@@ -1,5 +1,20 @@
 package server.booking.control;
 
+import server.planning.model.FishingSafariCatalog;
+import server.customer.model.CustomerRegister;
+import server.booking.model.BookingCatalogue;
+
+
 public class BookingController {
+
+	FishingSafariCatalogue fishingSafariCatalogue;
+	CustomerRegister customerRegister;
+	BookingCatalogue bookingCatalogue;
+
+	public BookingController(FishingSafariCatalogue fsc, CustomerRegister cr, BookingCatalogue bc){
+      fishingSafariCatalogue = fsc;
+      customerRegister = cr;
+      bookingCatalogue = bc;
+	}
 
 }
