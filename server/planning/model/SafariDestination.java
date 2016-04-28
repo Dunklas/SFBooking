5c7fcbe7f10d3572200e22c1a84722f302d0ecfd
alajv <williamlidholm1@gmail.com>
2016-04-28 11:22:13 +0200

    lite grejer

--- server/planning/model/SafariDestination.java
+++ server/planning/model/SafariDestination.java
@@ -40,7 +40,7 @@ public class SafariDestination{
 	return guide;
     }
 
-    public ArrayList<String> getTerrain(){
+    public ArrayList<String> getTerrainList(){
 	ArrayList<String> parsedTerrain = new ArrayList<String>();
   Scanner parser = new Scanner(this.terrain).useDelimiter(";");
   while(parser.hasNext()){
@@ -49,6 +49,9 @@ public class SafariDestination{
   return parsedTerrain;    
     }
     
+	public String getTerrain(){
+		return terrain;
+	}
 
     public boolean getActive(){
 	return active;
@@ -57,31 +60,66 @@ public class SafariDestination{
 
     public void setTerrain(String newTerrain){
 	this.terrain = newTerrain;
+	try{
+	    Update dbUpdate = new Update();
+	    dbUpdate.updateTerrain(this.location, this.terrain);
+	} catch (Exception ex){
+	    ex.printStackTrace();
+	}
     }
 
     public void setGuide(String newGuide) {
 	this.guide = newGuide;
+	try{
+	Update dbUpdate = new Update();
+	dbUpdate.updateGuide(this.location, this.guide);
+	} catch (Exception ex){
+	    ex.printStackTrace();
+	}
     }
 
     public void setEquipmentReq(String newEquipment){
 	this.equipmentReq = newEquipment;
+	try{
+	    Update dbUpdate = new Update();
+	    dbUpdate.updateEquipmentReq(this.location, this.equipmentReq);
+	} catch (Exception ex){
+	    ex.printStackTrace();
+	}
     }
    
     public void setMaxParticipants(int newMaxParticipants){
-	this.maxParticipants = newMaxParticipants;   
+	this.maxParticipants = newMaxParticipants;
+	try{
+	    Update dbUpdate = new Update();
+	    dbUpdate.updateMaxParticipants(this.location, this.maxParticipants);
+	} catch (Exception ex){
+	    ex.printStackTrace();
+	}
     }
 
     public void setStatus(Boolean active){
 	this.active = active;
+	try{
+	    Update dbUpdate = new Update();
+	    dbUpdate.updateStatus(this.location, this.active);
+	} catch (Exception ex){
+	    ex.printStackTrace();
+	}
     }
- 
     /**
+    public void updateSafariDestination(String location, String equipmentReq, int maxParticipants, String guide, String terrain, boolean active) throws SQLException{
+	Update dbUpdate = new Update();
+	dbUpdate.updateSafariDestination(location, equipmentReq, maxParticipants, guide, terrain, active);
+	System.out.println("Kom in i metodanrop i safari destination modellen");
+    }
+    */
+
     public String getSafariDestinationElement(String location, String column) throws SQLException{
 	Select dbSelect = new Select();
 	String element = dbSelect.selectSafariDestinationElement(location, column);
 	return element;
 
-	}*/
-
+    }
 
 }//End of class
