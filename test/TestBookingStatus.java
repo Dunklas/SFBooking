8a4d17fe6c30b86a2cc680b5d68479005620385c
Rickard <dunklas@gmail.com>
2016-05-10 13:35:16 +0200

    Payment test...

---
+++ test/TestBookingStatus.java
@@ -0,0 +1,14 @@
+package test;
+
+import server.booking.handler.*;
+
+public class TestBookingStatus {
+
+	BookingHandler bh = new BookingHandler();
+
+	public static void main(String[] args) {
+		bh.paymentCheck();
+	}
+
+	
+}
