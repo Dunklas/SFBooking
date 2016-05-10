3a03c434bcc3b7a8366dcabbe86bcced546cb883
Rickard <dunklas@gmail.com>
2016-05-10 13:21:05 +0200

    Updated test file for Booking

--- server/utils/storage/BookingStorage.java
+++ server/utils/storage/BookingStorage.java
@@ -7,15 +7,15 @@ import java.util.*;
 
 public interface BookingStorage{
 	
-   void put(Booking b);
+   void put(Booking b) throws StorageException;
 
-   ArrayList<Booking> getByStatus(int status);
+   ArrayList<Booking> getByStatus(int status) throws StorageException;
 
-   Booking get(int id);
+   Booking get(int id) throws StorageException;
 
-   ArrayList<Booking> get(Customer c);
+   ArrayList<Booking> get(Customer c) throws StorageException;
 
-   ArrayList<Booking> get(FishingSafari fs);
+   ArrayList<Booking> get(FishingSafari fs) throws StorageException;
 
 
 }
--- test/TestBookingStorage.java
+++ test/TestBookingStorage.java
@@ -1,10 +1,38 @@
 package test;
 
+import server.utils.storage.*;
+import server.customer.model.*;
+import server.planning.model.*;
+import server.booking.model.*;
+
 public class TestBookingStorage {
 
     private FishingSafariStorage fStore = FishingSafariStorageFactory.getStorage();
     private SafariDestinationStorage sdStore = SafariDestinationStorageFactory.getStorage();
     private BookingStorage bStore = BookingStorageFactory.getStorage();
+    private CustomerStorage cStore = CustomerStorageFactory.getStorage();
+
+    public TestBookingStorage() {
+	    FishingSafari f1 = fStore.get(21);
+	    Customer c1 = null;
+	    try {
+		    c1 = cStore.get("wille@wille.com");
+	    } catch (StorageException ex) {
+		    System.out.println(ex.getMessage());
+	    }
+
+	    Booking tempBook = new Booking(f1, c1, 2);
+
+	    try {
+		    bStore.put(tempBook);
+	    } catch (StorageException ex) {
+		    System.out.println(ex.getMessage());
+	    }
+    }
+
+    public static void main(String[] args) {
+	    new TestBookingStorage();
+    }
 
 
 }
