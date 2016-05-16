45765e1183a743ae0bb79b2f80c668c22da69138
christoffermagnusson <christoffer88@live.se>
2016-05-16 16:00:45 +0200

    Added MainWindow and MainController

---
+++ client/control/MainController.java
@@ -0,0 +1,10 @@
+package client.control;
+
+import client.ui.*;
+import client.control.*;
+import java.util.HashMap;
+
+public class MainController{
+  
+  
+}
--- client/ui/MainWindow.java
+++ client/ui/MainWindow.java
@@ -1,86 +1,128 @@
-package client.ui;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.GridBagConstraints;
 
-import javax.swing.*;
-import server.planning.control.*;
-import server.planning.model.*;
-import java.sql.*;
-import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.event.*;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JComponent;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JPanel {
 
-	/*
-	 * Initiate Model,View and Controllers
-	 **/
-	SafariDestinationView sdv = new SafariDestinationView();
-	SafariDestinationCatalog sdc = new SafariDestinationCatalog();
-    ModifySafariDestinationView msdv = new ModifySafariDestinationView();
-	SafariDestinationController sdcontroller = new SafariDestinationController(sdv, sdc,msdv);
-	
-	FishingSafariTopView fstv = new FishingSafariTopView();
-	FishingSafariBottomView fsbv = new FishingSafariBottomView();
-	ModifyFishingSafariView mfsv = new ModifyFishingSafariView();
-	FishingSafariCatalog fsc = new FishingSafariCatalog();
-	FishingSafariController fscd = new FishingSafariController(fstv,fsbv,mfsv,fsc,sdc); //ok att anv'a'nda samma sdc-objekt?
-  MainSplitView split;
+	ArrayList<JComponent> compArray = new ArrayList<JComponent>();
+	HashMap<String,JComponent> compMap = new HashMap<String,JComponent>();
 
-  JButton safariButton;
-  JButton fishingButton;
-    public static void main (String [] args){
-	
-    new MainWindow();
-
-    }
-	
-	public MainWindow(){
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(new Dimension(1000,800));
+	public MainWindow() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{38, 422, 91, 115, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
-		safariButton = new JButton("Safarimålshanterare");
-		fishingButton = new JButton("Fiskesafarihanterare");
-		safariButton.addActionListener(mainListener);
-		fishingButton.addActionListener(mainListener);
+		JPanel topPanel = new JPanel();
+		topPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		GridBagConstraints gbc_topPanel = new GridBagConstraints();
+		gbc_topPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_topPanel.fill = GridBagConstraints.BOTH;
+		gbc_topPanel.gridx = 0;
+		gbc_topPanel.gridy = 0;
+		add(topPanel, gbc_topPanel);
+		
+		JLabel lblSmlndskaFiskefrbundet = new JLabel("Sm\u00E5l\u00E4ndska Fiskef\u00F6rbundet");
+		lblSmlndskaFiskefrbundet.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		topPanel.add(lblSmlndskaFiskefrbundet);
+		
+		JPanel middlePanel = new JPanel();
+		GridBagConstraints gbc_middlePanel = new GridBagConstraints();
+		gbc_middlePanel.insets = new Insets(0, 0, 5, 0);
+		gbc_middlePanel.fill = GridBagConstraints.BOTH;
+		gbc_middlePanel.gridx = 0;
+		gbc_middlePanel.gridy = 1;
+		add(middlePanel, gbc_middlePanel);
+		
+		JPanel topButtonPanel = new JPanel();
+		GridBagConstraints gbc_topButtonPanel = new GridBagConstraints();
+		gbc_topButtonPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_topButtonPanel.fill = GridBagConstraints.BOTH;
+		gbc_topButtonPanel.gridx = 0;
+		gbc_topButtonPanel.gridy = 2;
+		add(topButtonPanel, gbc_topButtonPanel);
+		topButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton newBookingButton = new JButton("Ny kundbokning");
+		newBookingButton.setName("newBookingButton");
+		compArray.add(newBookingButton);
+		newBookingButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		topButtonPanel.add(newBookingButton);
+		
+		JButton handleBookingButton = new JButton("Hantera bokning");
+		handleBookingButton.setName("handleBookingButton");
+		compArray.add(handleBookingButton);
+		handleBookingButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		topButtonPanel.add(handleBookingButton);
+		
+		JPanel bottomPanelButton = new JPanel();
+		GridBagConstraints gbc_bottomPanelButton = new GridBagConstraints();
+		gbc_bottomPanelButton.fill = GridBagConstraints.BOTH;
+		gbc_bottomPanelButton.gridx = 0;
+		gbc_bottomPanelButton.gridy = 3;
+		add(bottomPanelButton, gbc_bottomPanelButton);
+		bottomPanelButton.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton handleFishingSafariButton = new JButton("Hantera fiskesafari");
+		handleFishingSafariButton.setName("handleFishingSafariButton");
+		compArray.add(handleFishingSafariButton);
+		handleFishingSafariButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bottomPanelButton.add(handleFishingSafariButton);
+		
+		JButton handleDestinationButton = new JButton("Hantera safarim\u00E5l");
+		handleDestinationButton.setName("handleDestinationButton");
+		handleDestinationButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bottomPanelButton.add(handleDestinationButton);
+		
+		JButton btnHanteraBoende = new JButton("Hantera boende");
+		btnHanteraBoende.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bottomPanelButton.add(btnHanteraBoende);
+		
+		JButton btnHanteraKund = new JButton("Hantera kunder");
+		btnHanteraKund.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bottomPanelButton.add(btnHanteraKund);
 
-		mainPanel.add(safariButton); mainPanel.add(fishingButton);
-
-		add(mainPanel);
 	}
 
-	ActionListener mainListener = new ActionListener(){
-		public void actionPerformed(ActionEvent e){
-     if(e.getSource()==safariButton){
-         split = new MainSplitView(msdv,sdv);
-         JFrame safariFrame = new JFrame();
-         safariFrame.setVisible(true);
-         safariFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-         safariFrame.setSize(1000,800);
-         safariFrame.add(split);
-     }
-     else if(e.getSource()==fishingButton){
-         JPanel mainFishingPanel = new JPanel();
-         mainFishingPanel.setLayout(new GridLayout(2,1));
-         mainFishingPanel.add(fstv);
-         mainFishingPanel.add(fsbv);
-
-         split = new MainSplitView(mfsv,mainFishingPanel);
-         JFrame fishingFrame = new JFrame();
-         fishingFrame.setVisible(true);
-         fishingFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-         fishingFrame.setSize(1000,800);
-         fishingFrame.add(split);
-     }  
+	private void initCompMap(){
+		for(JComponent comp : compArray){
+			compMap.put(comp.getName(),comp);
 		}
-	};
-
-
-	
-
-
-
+	}
+	public HashMap<String,JComponent> getCompMap(){
+		return compMap;
+	}
+	private JLabel putImage(String filepath){
+		JLabel picLabel = null;
+		try{
+			BufferedImage picture = ImageIO.read(new File(filepath));
+			picLabel = new JLabel(new ImageIcon(picture));
+		}
+		catch(IOException e){
+			//...
+		}
+		return picLabel;
+	}
 
 }
